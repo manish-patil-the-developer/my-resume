efd5164f73cd4d0ce84a3ef7df921d5076a0a258
manish-patil-the-developer <73695994+manish-patil-the-developer@users.noreply.github.com>
2020-10-30 18:26:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,257 @@
+<?php
+include_once("header.php");
+?>
+<div class="d-flex ">
+    <section class="d-flex flex-column w-100">
+        <h2 class="flex-column my-0 mx-auto justify-content-center">
+            <span class="text-primary">manish</span> Patil.
+        </h2>
+        <p class="my-0 mx-auto p-0 justify-content-center">
+            <span class="text-primary">Front-End</span> <strong>and</strong> <span class="text-primary">Back-End</span> <strong>Developer</strong>.
+        </p>
+        <hr class="m-0">
+    </section>
+ </div>
+<div class="row">
+    <div class="col-12 col-lg-6 colsection border pl-3 pr-2">
+        <section class="py-3" id="experience">
+            <div class="resume-section-content">
+                <h3 class="m-2 ml-0">Experience</h3>
+                <div class="d-flex flex-column flex-md-row justify-content-between my-2">
+                    <div class="flex-grow-1">
+                        <h5 class="d-flex mb-0 section_title"> <span class="border-right border-success pr-2 mr-2">2</span> 360 Web Services PVT LTD<span class="text-primary ml-auto">SEPTEMBER – 2018</span>&nbsp;to&nbsp;<span class="text-primary"> MARCH – 2020</span></h5>
+                        <div class="subheading mb-1">Backend Developer </div>
+                        <hr class="m-0 w-75">
+                        <p class="m-0 p-0 section_detail">
+                            <i class="fas fa-quote-left"></i> PHP <i class="fas fa-quote-right"></i>&nbsp;
+                            <i class="fas fa-quote-left"></i> Java Script / JQuery <i class="fas fa-quote-right"></i>&nbsp;
+                            <i class="fas fa-quote-left"></i> Slim PHP <i class="fas fa-quote-right"></i> &nbsp;
+                            <i class="fas fa-quote-left"></i> Codeigniter <i class="fas fa-quote-right"></i> &nbsp;
+                        </p>
+                        <hr class="m-0 w-75">
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Handling form submission of any Different Form,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Creating customize CMS or Web Application,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Working on payment gateway – PayPal and Stripe,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Manipulating WordPress files to customize theme,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Creating API for Third parties,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Displaying data from database to Front End,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Manipulating DOM to display required Output.</p>
+                    </div>
+                </div>
+                <hr class="m-0">
+                <div class="d-flex flex-column flex-md-row justify-content-between my-2">
+                    <div class="flex-grow-1">
+                        <h5 class="d-flex mb-0 section_title"><span class="border-right border-success pr-2 mr-2">1</span> Anand India PVT LTD<span class="text-primary ml-auto">MAY – 2014</span>&nbsp;to&nbsp;<span class="text-primary"> JULY – 2018</span></h5>
+                        <div class="subheading mb-1">Senior Franchise Coordinator</div>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Introducing Clients or franchisee,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Managing Team to resolve franchise’s issue,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Create process which can save company’s / Franchise’s /Operational teams’ Time,</p>
+                        <p class="m-0 py-1 section_detail"><i class="fa fa-check" aria-hidden="true"></i> Managing logistic of franchisee.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0">
+        <section class="py-3" id="freeLance_project">
+            <div class="resume-section-content">
+                <h3 class="m-2 ml-0">FreeLance Project</h3>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
+                    <div class="flex-grow-1">
+                        <h5 class="d-flex mb-1">Agri Samaj Mira Bhayander<span class="ml-auto">status - <strong class="text-primary"> UNDER DEVELOPMENT </strong></span></h5>
+                        <div class="subheading mb-1">URL - <strong><a href="http://agrisamajmirabhayander.com/" target="_blank">agrisamajmirabhayander.com </a></strong> </div>
+                        <p class="m-0 py-1 section_detail"><i class="far fa-comment"></i> Content pending from client side,</p>
+                        <p class="m-0 py-1 section_detail"><i class="far fa-comment"></i> Pending final confirmation to design and last changes.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0">
+        <section class="py-3" id="college_project">
+            <div class="resume-section-content">
+                <h3 class="m-2 ml-0">College Project</h3>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
+                    <div class="flex-grow-1">
+                        <h5 class="d-flex mb-1">Color fax Company of Garment</h5>   
+                        <!-- <span class="ml-auto">status - <strong class="text-primary"> UNDER DEVELOPMENT </strong></span> -->
+                        <div class="subheading mb-1">Role - Developer </div>
+                        <p class="m-0 p-0 section_detail"><i class="fa fa-bullseye"></i> It’s a dynamic Web site for Color Fax Company of garment who want to sell their product online,</p>
+                        <p class="m-0 p-0 section_detail"><i class="fa fa-bullseye"></i> interested to keep some database online of client and vendors.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0">
+        <section class="py-3" id="education">
+            <div class="resume-section-content">
+                <h3 class="m-2 ml-0">Education</h3>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
+                    <div class="flex-grow-1">
+                    <h5 class="d-flex mb-0">B.SC.IT<span class="text-primary ml-auto">APRIL -2014</span>&nbsp;with&nbsp;<span class="text-primary"> 57.35%</span></h5>
+                        <div class="subheading mb-0">Mumbai University </div>
+                        <!-- <p>Handling form submission of newsletter and contact us form, creating CRM, Displaying data from database to Front End, Creating API.</p> -->
+                    </div>
+                    <!-- <div class="flex-shrink-0"><span class="text-primary">September – 2018 to March – 2020</span></div> -->
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
+                    <div class="flex-grow-1">
+                        <h5 class="d-flex mb-0">HSC<span class="text-primary ml-auto">Feb.2010</span>&nbsp;with&nbsp;<span class="text-primary"> 54.50%</span></h5>
+                        <div class="subheading mb-0">Maharashtra Board</div>
+                        <!-- <p>Introducing Clients or franchisee, Coordination between company and franchisee.</p> -->
+                    </div>
+                    <!-- <div class="flex-shrink-0"></div> -->
+                </div>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
+                    <div class="flex-grow-1">
+                    <h5 class="d-flex mb-0">SSC<span class="text-primary ml-auto">MAR.2008</span>&nbsp;with&nbsp;<span class="text-primary"> 63.7%</span></h5>
+                        <div class="subheading mb-0">Maharashtra Board</div>
+                        <!-- <p>Introducing Clients or franchisee, Coordination between company and franchisee.</p> -->
+                    </div>
+                    <!-- <div class="flex-shrink-0"></div> -->
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div class="col-12 col-lg-6 colsection border pl-2">
+        <section class="py-3" id="skills">
+            <div class="resume-section-content">
+                <h3 class="m-2 ml-0">Skills</h3>
+                <div class="d-flex flex-row justify-content-around flex-wrap">
+                    <span class="label label-primary px-3">PHP</span>
+                    <span class="label label-primary px-3">Java Script</span>
+                    <span class="label label-primary px-3">Ajax</span>
+                    <span class="label label-primary px-3">JQuery</span>
+                    <span class="label label-primary px-3">HTML</span>
+                    <span class="label label-primary px-3">CSS</span>
+                </div>
+                <hr class="my-2">
+
+                <h3 class="m-2 ml-0">Frameworks</h3>
+                <div class="d-flex flex-row justify-content-start flex-wrap">
+                    <span class="label label-primary mx-3 px-2">Slim PHP</span>
+                    <span class="label label-primary mx-3 px-2">Codeigniter</span>
+                </div>
+
+                <h3 class="m-2 ml-0">CMS</h3>
+                <div class="d-flex flex-row justify-content-start flex-wrap">
+                    <span class="label label-primary mx-3 px-2">Wordpress</span>
+                </div>
+
+                <!-- <div class="d-flex">
+                    <div class="col-6 p-0">
+                        <h3 class="ml-2 mt-0 mr-0 mb-2">Frameworks</h3>
+                        <div class="d-flex flex-column flex-md-row justify-content-around flex-wrap">
+                            <span class="label label-primary">Slim PHP</span>
+                            <span class="label label-primary">Codeigniter</span>
+                        </div>
+                    </div>
+
+                    <div class="col-6 border-left">
+                        <h3 class="ml-0 mt-0 mr-0 mb-2">CMS</h3>
+                        <div class="d-flex flex-column flex-md-row flex-wrap">
+                            <span class="label label-primary">Wordpress</span>
+                        </div>
+                    </div>
+                </div> -->
+                <hr class="my-2">
+                <div class="d-flex">
+                    <div class="col-12 p-0">
+                        <h3 class="ml-2 mt-0 mr-0 mb-2">Languages</h3>
+                        <div class="d-flex flex-column flex-md-row justify-content-around flex-wrap">
+                            <p class="m-0 px-2 section_detail">
+                                <i class="far fa-comment"></i> English, 
+                                <i class="far fa-comment"></i> Marathi, 
+                                <i class="far fa-comment"></i> Hindi, 
+                                <strong>- Professional working proficiency</strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>  
+        <hr class="m-0"> 
+        <section class="py-3" id="objective">
+            <div class="resume-section-content">
+                <h3 class="m-2 ml-0">Objective</h3>
+                <div class="d-flex flex-column flex-md-row justify-content-between mb-2">
+                    <div class="flex-grow-1">
+                        <ul class=" list-group list-group-flush m-0 p-0 mx-auto justify-content-center px-1">
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-crosshairs"></i> To enhance knowledge and skills as much as I can, which helps me in contributing to the success of a company. </li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-crosshairs"></i> To enhance <strong class="text-primary">1 year 6 months</strong> of experience as Backend or PHP Developer.</li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-crosshairs"></i> To enhance <strong class="text-primary">4 year 2 months</strong> of experience as Sales or Senior Franchise Coordinator.</li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-crosshairs"></i> Be strong - Make strong <strong class="text-primary">My Self </strong> and <strong class="text-primary">My Surrounding Space.</strong></li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-crosshairs"></i> To be  <strong class="text-primary">Limitless </strong>.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0">
+        <section class="py-3" id="strengths">
+            <div class="resume-section-content">
+                <h3 class="m-2 ml-0">Strengths</h3>
+                <div class="d-flex flex-column flex-md-row justify-content-around flex-wrap">
+                <div class="flex-grow-1">
+                        <ul class=" list-group list-group-flush m-0 p-0 mx-auto justify-content-center px-1">
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-fist-raised"></i> An individual with a positive attitude with excellent time management skills. </li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-fist-raised"></i> Eager to learn and gain New Things and Experience.</li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-fist-raised"></i> Good Research at work, ability to give dedicated efforts.</li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-fist-raised"></i> Determined and focused on achieving goals. </li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-fist-raised"></i> Decision making ability.</li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-fist-raised"></i> Full commitment to work.</li>
+                            <li class="list-group-item border-0 m-0 p-1 colsection section_detail"><i class="fas fa-fist-raised"></i> Team Player and Quick Learner.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <hr class="m-0">
+        <section class="py-3" id="personal_information">
+            <div class="resume-section-content">
+                <h3 class="m-2 ml-0">Personal Information</h3>
+                <div class="d-flex flex-column flex-md-row justify-content-around flex-wrap">
+                    <table class="table table-sm table-borderless">
+                        <tbody class="section_detail">
+                            <tr>
+                                <th scope="row">Date Of Birth :</th>
+                                <td>29 August 1992</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Gender :</th>
+                                <td>Male.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Address :</th>
+                                <td>Bhayandar (E) 401105.</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Contact No :</th>
+                                <td> <strong class="text-primary">8097891055</strong></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Email ID :</th>
+                                <td> <strong class="text-primary">manish.patil092@gmail.com</strong></td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Nationality :</th>
+                                <td> Indian</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Nationality :</th>
+                                <td> Indian</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Hobbies :</th>
+                                <td> Dancing, Listening Music, Playing Football, Social Networking</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>        
+    </div>
+
+</div>
+<?php
+include_once("footer.php");
+?>
