3d1b0a4d2f40e81075a064196b6e483ec7ceb3e1
manish patil <manish.patil092@gmail.com>
2024-05-05 18:58:02 +0530

    second commit for fixing UI

--- header.php
+++ header.php
@@ -7,12 +7,11 @@
         <meta name="author" content="" />
         <title>Resume - Start Bootstrap Theme</title>
         <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
         <script src="js/all.js"></script>
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
+        <!-- Core theme CSS-->
         <link href="./css/styles.css" rel="stylesheet" />
         <link href="./css/main.css" rel="stylesheet" />
     </head>
